c00eb4166a241d9e32ae01c7ec46b1afb86d595e
xcxz1994 <981877654@qq.com>
2018-05-01 18:10:03 +0800

    #下一步是等级管理的图表

--- member-Grading.php
+++ member-Grading.php
@@ -1,3 +1,141 @@
+<?php
+ini_set("error_reporting","E_ALL & ~E_NOTICE");
+
+require_once './include.php';
+
+$rows=getAllUser();
+$Allnumber=count($rows);
+$commonNum=getcommon()[1];
+$IroncardNum=getIroncard()[1];
+$CopperNum=getCopper()[1];
+$SilverNum=getSilver()[1];
+$GoldNum=getGold()[1];
+$JewelNum=getJewel()[1];
+$RedJewelNum=getRedJewel()[1];
+$BlueJewelNum=getBlueJewel()[1];
+$BlackJewelNum=getBlackJewel()[1];
+
+function getcommon(){
+    $common=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='普通会员') {
+            $common.array_push($common,$rows[$i]);
+        }else{
+           // echo "没有普通会员";
+        }
+    }
+    $commonNum=count($common);
+    return array($common,$commonNum);
+}
+function getIroncard(){
+    $Ironcard=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='铁牌会员') {
+            $Ironcard.array_push($Ironcard,$rows[$i]);
+        }else{
+           // echo "没有铁牌会员";
+        }
+    }
+    $IroncardNum=count($Ironcard);
+    return array($Ironcard,$IroncardNum);
+}
+function getCopper(){
+    $Copper=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='铜牌会员') {
+            $Copper.array_push($Copper,$rows[$i]);
+        }else{
+            //echo "没有铜牌会员";
+        }
+    }
+    $CopperNum=count($Copper);
+    return array($Copper,$CopperNum);
+}
+function getSilver(){
+    $Silver=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='银牌会员') {
+            $Silver.array_push($Silver,$rows[$i]);
+        }else{
+            //echo "没有银牌会员";
+        }
+    }
+    $SilverNum=count($Silver);
+    return array($Silver,$SilverNum);
+}
+function getGold(){
+    $Gold=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='金牌会员') {
+            $Gold.array_push($Gold,$rows[$i]);
+        }else{
+            //echo "没有金牌会员";
+        }
+    }
+    $GoldNum=count($Gold);
+    return array($Gold,$GoldNum);
+}
+function getJewel(){
+    $Jewel=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='钻石会员') {
+            $Jewel.array_push($Jewel,$rows[$i]);
+        }else{
+            //echo "没有钻石会员";
+        }
+    }
+    $JewelNum=count($Jewel);
+    return array($Jewel,$JewelNum);
+}
+function getRedJewel(){
+    $RedJewel=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='红钻会员') {
+            $RedJewel.array_push($RedJewel,$rows[$i]);
+        }else{
+            //echo "没有红钻会员";
+        }
+    }
+    $RedJewelNum=count($RedJewel);
+    return array($RedJewel,$RedJewelNum);
+}
+function getBlueJewel(){
+    $BlueJewel=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='蓝钻会员') {
+            $BlueJewel.array_push($BlueJewel,$rows[$i]);
+        }else{
+            //echo "没有蓝钻会员";
+        }
+    }
+    $BlueJewelNum=count($BlueJewel);
+    return array($BlueJewel,$BlueJewelNum);
+}
+function getBlackJewel(){
+    $BlackJewel=array();
+    $rows=getAllUser();
+    for($i=0;$i<count($rows);$i++){
+        if($rows[$i]['cl_grade']=='黑钻会员') {
+            $BlackJewel.array_push($BlackJewel,$rows[$i]);
+        }else{
+            //echo "没有黑钻会员";
+        }
+    }
+    $BlackJewelNum=count($BlackJewel);
+    return array($BlackJewel,$BlackJewelNum);
+}
+
+
+?>
+
 <!DOCTYPE>
 <html >
 <head>
@@ -38,16 +176,16 @@
       </div>
       <div class="widget-body">
          <ul class="b_P_Sort_list">
-             <li><i class="orange  fa fa-user-secret"></i><a href="#">全部(235)</a></li>
-             <li><i class="fa fa-diamond pink "></i> <a href="#">普通会员(235)</a></li>
-             <li> <i class="fa fa-diamond pink "></i> <a href="#">铁牌会员(2215)</a> </li>
-             <li> <i class="fa fa-diamond pink "></i> <a href="#">铜牌会员(3456)</a></li>
-             <li><i class="fa fa-diamond pink "></i> <a href="#">银牌会员(4332)</a></li>
-             <li><i class="fa fa-diamond pink "></i> <a href="#">金牌会员(1332)</a></li>
-             <li> <i class="fa fa-diamond grey"></i> <a href="#">钻石会员(4543)</a></li>
-             <li> <i class="fa fa-diamond red"></i> <a href="#">红钻会员(343)</a></li>
-             <li> <i class="fa fa-diamond blue"></i> <a href="#">蓝钻会员(2343)</a></li>
-             <li> <i class="fa fa-diamond grey"></i> <a href="#">黑钻2(53)</a></li>
+             <li><i class="orange  fa fa-user-secret"></i><a href="#">全部(<?php echo $Allnumber;?>)</a></li>
+             <li><i class="fa fa-diamond pink "></i> <a href="#">普通会员(<?php echo $commonNum;?>)</a></li>
+             <li> <i class="fa fa-diamond pink "></i> <a href="#">铁牌会员(<?php echo $IroncardNum;?>)</a> </li>
+             <li> <i class="fa fa-diamond pink "></i> <a href="#">铜牌会员(<?php echo $CopperNum;?>)</a></li>
+             <li><i class="fa fa-diamond pink "></i> <a href="#">银牌会员(<?php echo $SilverNum;?>)</a></li>
+             <li><i class="fa fa-diamond pink "></i> <a href="#">金牌会员(<?php echo $GoldNum;?>)</a></li>
+             <li> <i class="fa fa-diamond grey"></i> <a href="#">钻石会员(<?php echo $JewelNum;?>)</a></li>
+             <li> <i class="fa fa-diamond red"></i> <a href="#">红钻会员(<?php echo $RedJewelNum;?>)</a></li>
+             <li> <i class="fa fa-diamond blue"></i> <a href="#">蓝钻会员(<?php echo $BlueJewelNum;?>)</a></li>
+             <li> <i class="fa fa-diamond grey"></i> <a href="#">黑钻(<?php echo $BlackJewelNum;?>)</a></li>
             </ul>
   </div>
   </div>
@@ -69,101 +207,38 @@
        <table class="table table-striped table-bordered table-hover" id="sample-table">
 		<thead>
 		 <tr>
-				<th width="25"><label><input type="checkbox" class="ace"><span class="lbl"></span></label></th>
-				<th width="80">ID</th>
-				<th width="100">用户名</th>
-				<th width="80">性别</th>
-				<th width="120">手机</th>
-				<th width="150">邮箱</th>
-				
-				<th width="180">加入时间</th>
-                <th width="100">等级</th>
-                <th width="100">积分</th>
-				<th width="70">状态</th>                
-				<th width="250">操作</th>
+             <th width="25"><label><input type="checkbox" class="ace"><span class="lbl"></span></label></th>
+             <th width="80">账号</th>
+             <th width="100">用户名</th>
+             <th width="100">法人代表</th>
+             <th width="90">手机</th>
+             <th width="100">地址</th>
+             <th width="100">加入时间</th>
+             <th width="100">等级</th>
+             <th width="70">状态</th>
+             <th width="80">操作</th>
 			</tr>
 		</thead>
 	<tbody>
-		<tr>
-          <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
-          <td>1</td>
-          <td><u style="cursor:pointer" class="text-primary" onclick="member_show('张三','member-show.html','10001','500','400')">张三</u></td>
-          <td>男</td>
-          <td>13000000000</td>
-          <td>admin@mail.com</td>         
-          <td>2014-6-11 11:11:42</td>
-          <td>普通用户</td>
-          <td class="text-l">345</td>
-          <td class="td-status"><span class="label label-success radius">已启用</span></td>
-          <td class="td-manage">
-          <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check bigger-120"></i></a> 
-          <a title="删除" href="javascript:;"  onclick="member_del(this,'1')" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
-          </td>
-		</tr>
+    <?php  foreach($rows as $row):?>
         <tr>
-          <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
-          <td>2</td>
-          <td><u style="cursor:pointer" class="text-primary" onclick="member_show('张小泉','member-show.html','1031','500','400')">张小泉</u></td>
-          <td>男</td>
-          <td>13000000000</td>
-          <td>admin@mail.com</td>
-          <td>2014-6-11 11:11:42</td>
-          <td>普通用户</td>
-           <td class="text-l">1345</td>
-          <td class="td-status"><span class="label label-success radius">已启用</span></td>
-          <td class="td-manage">
-          <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check bigger-120"></i></a>       
-          <a title="删除" href="javascript:;"  onclick="member_del(this,'1')" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
-          </td>
-		</tr>
-         <tr>
-          <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
-          <td>3</td>
-          <td><u style="cursor:pointer" class="text-primary" onclick="member_show('张小泉','member-show.html','10301','500','400')">张小泉</u></td>
-          <td>男</td>
-          <td>13000000000</td>
-          <td>admin@mail.com</td>
-          <td>2014-6-11 11:11:42</td>
-          <td>银牌用户</td>
-           <td class="text-l">645</td>
-          <td class="td-status"><span class="label label-success radius">已启用</span></td>
-          <td class="td-manage">
-          <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check bigger-120"></i></a>         
-          <a title="删除" href="javascript:;"  onclick="member_del(this,'1')" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
-          </td>
-		</tr>
-         <tr>
-          <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
-          <td>4</td>
-          <td><u style="cursor:pointer" class="text-primary" onclick="member_show('张小泉','member-show.html','10001','500','400')">张小泉</u></td>
-          <td>男</td>
-          <td>13000000000</td>
-          <td>admin@mail.com</td>
-          <td>2014-6-11 11:11:42</td>
-          <td>银牌用户</td>
-           <td class="text-l">645</td>
-          <td class="td-status"><span class="label label-success radius">已启用</span></td>
-          <td class="td-manage">
-          <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check bigger-120"></i></a>       
-          <a title="删除" href="javascript:;"  onclick="member_del(this,'1')" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
-          </td>
-		</tr>
-         <tr>
-          <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
-          <td>5</td>
-          <td><u style="cursor:pointer" class="text-primary" onclick="member_show('张小泉','member-show.html','10001','500','400')">张小泉</u></td>
-          <td>男</td>
-          <td>13000000000</td>
-          <td>admin@mail.com</td>
-          <td>2014-6-11 11:11:42</td>
-          <td>银牌用户</td>
-           <td class="text-l">345</td>
-          <td class="td-status"><span class="label label-success radius">已启用</span></td>
-          <td class="td-manage">
-          <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check  bigger-120"></i></a>        
-          <a title="删除" href="javascript:;"  onclick="member_del(this,'1')" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
-          </td>
-		</tr>
+            <td><label><input type="checkbox" class="ace"><span class="lbl"></span></label></td>
+            <td><?php echo $row['cl_id'];?></td>
+            <td><u style="cursor:pointer" class="text-primary" onclick="member_show(<?php echo $row['id'];?>)"><?php echo $row['cl_name'];?></u></td>
+            <td><?php echo $row['cl_represent'];?></td>
+
+            <td><?php echo $row['cl_phone'];?></td>
+            <td class="text-l"><?php echo $row['cl_address'];?></td>
+            <td><?php echo $row['cl_registDate'];?></td>
+            <td><?php echo $row['cl_grade'];?></td>
+            <td class="td-status"><span class="label label-success radius">已启用</span></td>
+            <td class="td-manage">
+                <a onClick="member_stop(this,'10001')"  href="javascript:;" title="停用"  class="btn btn-xs btn-success"><i class="fa fa-check bigger-120"></i></a>
+
+                <a title="删除" href="javascript:;"  onclick="member_del(this,<?php echo $row['id'];?>)" class="btn btn-xs btn-warning" ><i class="fa fa-trash  bigger-120"></i></a>
+            </td>
+        </tr>
+    <?php endforeach;?>
       </tbody>
 	</table>
    </div>
