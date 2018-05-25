cdf8e7755ff94f9672ad02bfd8749c1be492ed90
xcxz1994 <981877654@qq.com>
2018-05-25 20:59:43 +0800

    #项目封笔

--- Adminindex.php
+++ Adminindex.php
@@ -457,7 +457,7 @@ checkLogined()
                 </ul>
             </div>
 
-            <iframe id="iframe" style="border:0; width:100%; background-color:#FFF;"name="iframe" frameborder="0" src="home.html">  </iframe>
+            <iframe id="iframe" style="border:0; width:100%; background-color:#FFF;"name="iframe" frameborder="0" src="Adminhome.php">  </iframe>
 
 
             <!-- /.page-content -->
--- Writerindex.php
+++ Writerindex.php
@@ -377,9 +377,6 @@ checkLogined()
                     <li><a href="#" class="dropdown-toggle"><i class="icon-desktop"></i><span class="menu-text"> 商品管理 </span><b class="arrow icon-angle-down"></b></a>
                         <ul class="submenu">
                             <li class="home"><a  href="javascript:void(0)" name="Products_List.php"  title="商品类表" class="iframeurl"><i class="icon-double-angle-right"></i>商品类表</a></li>
-                            <li class="home"><a  href="javascript:void(0)" name="Attribute_Manage.php" title="属性管理"  class="iframeurl"><i class="icon-double-angle-right"></i>物料属性管理</a></li>
-                            <li class="home"><a href="javascript:void(0)" name="Category_Manage.php" title="分类管理"  class="iframeurl"><i class="icon-double-angle-right"></i>分类管理</a></li>
-
                         </ul>
                     </li>
 
@@ -432,7 +429,7 @@ checkLogined()
                 </ul>
             </div>
 
-            <iframe id="iframe" style="border:0; width:100%; background-color:#FFF;"name="iframe" frameborder="0" src="home.html">  </iframe>
+            <iframe id="iframe" style="border:0; width:100%; background-color:#FFF;"name="iframe" frameborder="0" src="Writerhome.php">  </iframe>
 
 
             <!-- /.page-content -->
