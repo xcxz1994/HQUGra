9529a9cf9536d927dba9cec834ee841ef577872e
xcxz1994 <981877654@qq.com>
2018-05-25 16:19:20 +0800

    #首页完善

--- home.php
+++ home.php
@@ -20,7 +20,7 @@ if(isset($_SESSION['adminId'])){
 }elseif(isset($_COOKIE['adminId'])){
     $sql="select * from sys_message where message_to={$_COOKIE['adminId']}and message_status=2";
     $rows=fetchAll($sql);
-    // print_r($row);
+    //print_r($rows);
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -54,7 +54,7 @@ if(isset($_SESSION['adminId'])){
 <div class="page-content clearfix">
  <div class="alert alert-block alert-success">
   <button type="button" class="close" data-dismiss="alert"><i class="icon-remove"></i></button>
-  <i class="icon-ok green"></i>欢迎使用<strong class="green">后台管理系统<small>(v1.2)</small></strong>,你本次登陆时间为2016年7月12日13时34分，登陆IP:192.168.1.110.	
+  <i class="icon-ok green"></i>欢迎使用<strong class="green">后台管理系统<small>(v1.2)</small></strong>,你本次登陆时间为<?php echo $showtime=date("Y-m-d H:i:s");?>，登陆IP为<?php echo $ip=getloginIP(); ?>
  </div>
  <div class="state-overview clearfix">
                   <div class="col-lg-3 col-sm-6">
